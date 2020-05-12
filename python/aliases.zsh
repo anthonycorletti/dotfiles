65b981e220aedd813524238de092fac78b28174a
Anthony Corletti <anthcor@gmail.com>
2020-05-12 13:58:44 -0400

    virtual env prompt

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -3,3 +3,11 @@ alias pyclean="find . -type f -name '*.py[co]' -delete -o -type d -name __pycach
 function pip-install-save {
     pip install $1 && pip freeze | grep -i $1 | awk '{ gsub("==", ">="); print $0}' >> requirements.txt
 }
+
+function newvenv {
+    python3 -m venv $1
+}
+
+function activate {
+    source "$1/bin/activate"
+}
--- python/env.zsh
+++ python/env.zsh
@@ -2,3 +2,4 @@
 # https://github.com/pyenv/pyenv/issues/1219#issuecomment-448658430
 export LDFLAGS="-L/usr/local/opt/zlib/lib -L/usr/local/opt/sqlite/lib"
 export CPPFLAGS="-I/usr/local/opt/zlib/include -I/usr/local/opt/sqlite/include"
+export VIRTUAL_ENV_DISABLE_PROMPT=1
