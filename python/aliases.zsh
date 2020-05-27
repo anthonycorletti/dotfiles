d646504680977c08c3dd4f12af813b07852de257
Anthony Corletti <anthcor@gmail.com>
2020-05-27 15:49:05 -0400

    deactivate

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,3 +1,4 @@
 alias pyclean="find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete"
 alias venv="python -m venv env"
 alias actv="source env/bin/activate"
+alias dctv="deactivate"
