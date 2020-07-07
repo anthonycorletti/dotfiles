714132c1a4c07a154477aa1560fd862049dc44a3
Anthony Corletti <anthcor@gmail.com>
2020-07-07 12:23:09 -0400

    cleanup gpg

--- gpg/install.sh
+++ gpg/install.sh
@@ -3,7 +3,7 @@ then
     echo "Checking if gpg agent is running"
     if [[ gpg-agent ]]; then
         echo "Installing pinentry mac for you."
-        # brew install pinentry-mac
-        # echo "pinentry-program /usr/local/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
+        brew install pinentry-mac
+        echo "pinentry-program /usr/local/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
     fi
 fi
