fa2bba418b88793d4b9c273efb73b6938d3a95e8
Anthony Corletti <anthcor@gmail.com>
2021-01-20 16:22:07 -0500

    small update

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,4 +1,4 @@
 alias pyclean="find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete -o -type f -name '.coverage.*' -delete"
-alias venv="python -m venv env"
-alias actv="source env/bin/activate"
+alias venv="python -m venv venv"
+alias actv="source venv/bin/activate"
 alias dctv="deactivate"
