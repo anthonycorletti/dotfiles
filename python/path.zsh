9ea194ed7137a5a141fcfb2d9b7d6c5b33a4faf4
Anthony Corletti <acorletti@algorithmia.io>
2021-02-04 13:00:07 -0500

    coo new

--- python/path.zsh
+++ python/path.zsh
@@ -1,6 +1,9 @@
 # some nice pyenv help
 export PYTHONSTARTUP="$HOME/.dotfiles/python/startup.py"
 
+# setup poetry
+export PATH="$HOME/.poetry/bin:$PATH"
+
 if [ $commands[pyenv] ]; then
     eval "$(pyenv init -)"
 fi
