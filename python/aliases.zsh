2d442a06637c741ba35eeb0a0c82656faa19bae7
Anthony Corletti <acorletti@algorithmia.io>
2021-02-08 10:22:34 -0500

    fix

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,4 +1,4 @@
 alias pyclean="find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete -o -type f -name '.coverage.*' -delete"
-alias venv="python -m .venv venv"
+alias venv="python -m venv .venv"
 alias actv="source .venv/bin/activate"
 alias dctv="deactivate"
