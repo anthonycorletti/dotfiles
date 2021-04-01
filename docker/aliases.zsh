b3aef2672ebc8108c2fcfb6a244843180b99a8d1
Anthony Corletti <anthcor@gmail.com>
2021-04-01 11:24:41 -0400

    wipeout alias

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,3 +1,4 @@
 alias d='docker $*'
 alias d-c='docker-compose $*'
 alias dockerclean='docker rm $(docker ps -a -q); docker rmi -f $(docker images --quiet --filter "dangling=true"); docker volume rm $(docker volume ls -q -f dangling=true);'
+alias dockerwipeout='docker rmi -f $(docker images -a -q)'
