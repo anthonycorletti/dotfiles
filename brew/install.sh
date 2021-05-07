0e55f55b5f1de257db67ce53ef4a849de2c9906e
Anthony Corletti <anthcor@gmail.com>
2021-05-07 00:47:57 -0400

    update git to use main

--- brew/install.sh
+++ brew/install.sh
@@ -14,7 +14,7 @@ then
   if test "$(uname)" = "Darwin"
   then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
   then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
   fi
