d887a487b62ac1b25aa07189f1cf35403dd8e5e4
Anthony Corletti <anthony@rungalileo.io>
2021-06-09 10:57:38 -0400

    faster better nicer listing

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -3,10 +3,7 @@
 #   `brew install coreutils`
 if $(gls &>/dev/null)
 then
-    alias ls="gls -F --color"
-    alias l="gls -lAh --color"
-    alias ll="gls -l --color"
-    alias la='gls -A --color'
+    alias l="gls -Ahl --group-directories-first --color"
 fi
 
 # Pipe my public key to my clipboard.
