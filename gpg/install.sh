ebc0bc5dc717e166ef72760c1bbb42680f16c0ff
Anthony Corletti <anthony@rungalileo.io>
2021-10-30 13:05:44 -0400

    update gpg setup

--- gpg/install.sh
+++ gpg/install.sh
@@ -1,12 +1,5 @@
-# this will likely fail on the first install of a fresh machine
-# install gpgsuite from https://gpgtools.org
-
 if ! type pinentry-mac > /dev/null;
 then
-    echo "Checking if gpg agent is running"
-    if [[ gpg-agent ]]; then
-        echo "Setting pinentry-program as pinentry-mac for gpg-agent. Checking if pinentry-mac is installed first."
-        brew install pinentry-mac
-        echo "pinentry-program /usr/local/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
-    fi
+    brew install pinentry-mac
+    echo "pinentry-program /usr/local/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
 fi
