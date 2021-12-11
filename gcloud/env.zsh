4532d7b73887c6bdb0f2a5e169d1b751c5246f53
Anthony Corletti <anthcor@gmail.com>
2021-12-11 15:39:44 -0500

    use home var

--- gcloud/env.zsh
+++ gcloud/env.zsh
@@ -4,4 +4,4 @@
 # this should be removed this as bq cli isn't ready for python3.8
 #
 
-export CLOUDSDK_PYTHON=/Users/anthcor/.pyenv/versions/3.7.8/bin/python3.7
+export CLOUDSDK_PYTHON=$HOME/.pyenv/versions/3.7.8/bin/python3.7
