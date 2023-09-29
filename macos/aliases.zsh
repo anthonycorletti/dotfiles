8c5e38cc6e81cb6945b5b49b97b884e6b84b5ab5
Anthony Corletti <anthcor@gmail.com>
2023-09-29 18:54:59 -0400

    small update

--- macos/aliases.zsh
+++ macos/aliases.zsh
@@ -1,4 +1,3 @@
 alias showhidden='defaults write com.apple.finder AppleShowAllFiles TRUE && killall Finder'
 alias hidehidden='defaults write com.apple.finder AppleShowAllFiles FALSE && killall Finder'
-alias lock="/System/Library/CoreServices/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine &> /dev/null"
-alias sleepnow="pmset sleepnow"
+alias lock="pmset sleepnow"
