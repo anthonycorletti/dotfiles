26a3b229fc4ad23bc486610519ab4c4110b87fb0
Anthony Corletti <anthcor@gmail.com>
2023-10-02 19:39:38 -0400

    dont need this with the new keyboard shortcut

--- macos/aliases.zsh
+++ macos/aliases.zsh
@@ -1,3 +1,2 @@
 alias showhidden='defaults write com.apple.finder AppleShowAllFiles TRUE && killall Finder'
 alias hidehidden='defaults write com.apple.finder AppleShowAllFiles FALSE && killall Finder'
-alias lock="pmset sleepnow"
