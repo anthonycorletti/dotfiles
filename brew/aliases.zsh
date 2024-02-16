603aa8aa289ff0968a3509aaa07d510abe615c8a
Anthony Corletti <anthcor@gmail.com>
2024-02-15 21:59:54 -0500

    small update for aliases

--- brew/aliases.zsh
+++
@@ -1,7 +0,0 @@
-# this is a workaround to have brew play nice with pyenv
-# https://github.com/pyenv/pyenv#homebrew-in-macos
-
-if [ $commands[pyenv] ];
-then
-    alias brew='env PATH="${PATH//$(pyenv root)\/shims:/}" brew'
-fi
