89ca5c3cc64d026420fcfb08de2ba558377c20fc
Anthony Corletti <anthcor@gmail.com>
2024-08-22 17:33:18 -0400

    pyenv to uv

--- python/install.sh
+++ python/install.sh
@@ -2,8 +2,9 @@
 #
 # Do some initial setup for Python
 #
-# We aren't going to install Python, that will be taken care of by pyenv
-# but we can do some initial setup here, like setting sane defaults for pip.
+# We aren't going to install Python, that will be taken care of by uv and brew,
+# but we can do some initial setup here, like setting sane defaults for pip if
+# we need to use it.
 
 pipconf="$HOME/.config/pip/pip.conf"
 mkdir -p "$(dirname "$pipconf")"
@@ -12,6 +13,7 @@ if [ ! -f "$pipconf" ]; then
   cat > "$pipconf" <<EOF
 [install]
 require-virtualenv = true
+
 [uninstall]
 require-virtualenv = true
 EOF
--- python/path.zsh
+++
@@ -1,5 +0,0 @@
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-
-eval "$(/opt/homebrew/bin/pyenv init --path)"
-eval "$(/opt/homebrew/bin/pyenv init -)"
