3d2e6bf03f7ce9252f77fdc9aed752b57dd55d99
Anthony Corletti <anthcor@gmail.com>
2024-08-30 15:26:01 -0400

    housekeeping

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,3 +1,3 @@
-alias nv="python -m venv .venv && source .venv/bin/activate && pip install --upgrade pip"
+alias av="source .venv/bin/activate && pip install --upgrade pip"
 alias dv="deactivate"
-alias av="source .venv/bin/activate"
+alias nv="python -m venv .venv && source .venv/bin/activate && pip install --upgrade pip"
