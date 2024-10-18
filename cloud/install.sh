63c5ab70abd2d3170fdb93c33a8b69a4143e16cb
Anthony Corletti <anthcor@gmail.com>
2024-10-17 23:18:51 -0400

    cleanup

--- cloud/install.sh
+++
@@ -1,4 +0,0 @@
-#
-# google cloud: install from source via https://cloud.google.com/sdk/docs/install-sdk
-# aws: install from source via https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
-# az: installed via brew
