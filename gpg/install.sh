e2388adb5d1c9f85d234136c21dcfe6f8d0d0ca4
Anthony Corletti <anthcor@gmail.com>
2024-11-04 17:59:43 -0500

    small updates

--- gpg/install.sh
+++ gpg/install.sh
@@ -1,5 +1,15 @@
-if ! command -v pinentry-mac &> /dev/null;
+GPG_AGENT_CONF=$HOME/.gnupg/gpg-agent.conf
+if command -v pinentry-mac &> /dev/null && command -v pinentry &> /dev/null;
+    echo "pinentry and pinentry-mac are installed"
 then
-    brew install pinentry-mac
-    echo "pinentry-program /opt/homebrew/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf 
+    if [ ! -f $GPG_AGENT_CONF ]; then
+        echo "no gpg-agent.conf file found, creating one"
+        echo "pinentry-program /opt/homebrew/bin/pinentry-mac" > $GPG_AGENT_CONF
+    else
+        echo "gpg-agent.conf file found"
+        if ! grep -q "pinentry-program /opt/homebrew/bin/pinentry-mac" $GPG_AGENT_CONF; then
+            print "pinentry-program /opt/homebrew/bin/pinentry-mac not found in gpg-agent.conf, adding it"
+            echo "pinentry-program /opt/homebrew/bin/pinentry-mac" > $GPG_AGENT_CONF
+        fi
+    fi
 fi
