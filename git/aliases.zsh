db1f514623c9c1b661c0aa8dbee7b75be468e03d
Anthony Corletti <anthcor@gmail.com>
2024-11-08 18:15:49 -0500

    Small style change.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -8,7 +8,7 @@ alias gcb='git copy-branch-name'
 alias gcl='git clone'
 alias gco='git checkout'
 alias gcom='git checkout $(gbm)'
-alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r' # Remove `+` and `-` from start of diff lines; just rely upon color.
+alias gd='git diff --color'
 alias ge='git-edit-new'
 alias gf='git fetch'
 alias gl="git log --pretty=oneline -n 23 --graph"
