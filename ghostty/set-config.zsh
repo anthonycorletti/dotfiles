2815c50a89e9a32c0aa8b9747e4bf8f879f22c20
Anthony Corletti <anthcor@gmail.com>
2024-12-16 14:39:52 +1100

    where were going we dont need configs

--- ghostty/set-config.zsh
+++
@@ -1,7 +0,0 @@
-cat <<EOF > ~/.config/ghostty/config
-cursor-style = underline
-macos-option-as-alt = left
-keybind = alt+left=esc:b
-keybind = alt+right=esc:f
-shell-integration-features = "no-cursor"
-EOF
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -2,31 +2,27 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
-setopt NO_BG_NICE # don't nice background tasks
-setopt NO_HUP
-setopt NO_LIST_BEEP
+setopt NO_BG_NICE # don't run background jobs at lower priority
+setopt NO_HUP # don't kill jobs when shell exits
+setopt NO_LIST_BEEP # don't beep on ambiguous completions
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS # allow functions to have local traps
-setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
+setopt HIST_VERIFY # show command with history expansion to be executed
+setopt SHARE_HISTORY # share history between sessions
 setopt EXTENDED_HISTORY # add timestamps to history
-setopt PROMPT_SUBST
-setopt CORRECT
-setopt COMPLETE_IN_WORD
-setopt IGNORE_EOF
+setopt PROMPT_SUBST # allow command substitution in prompt
+setopt CORRECT # autocorrect typos
+setopt COMPLETE_IN_WORD # allow completion in the middle of a word
+setopt IGNORE_EOF # don't exit on EOF
 
 setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
-setopt HIST_REDUCE_BLANKS
+setopt HIST_REDUCE_BLANKS # remove leading/trailing blanks from history lines
 
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
 setopt complete_aliases
 
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^?' backward-delete-char
+bindkey "^[[1;3C" forward-word
+bindkey "^[[1;3D" backward-word
