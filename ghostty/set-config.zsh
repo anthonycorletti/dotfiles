d96db91da146257c0d2c38f50310ba120173f604
Anthony Corletti <anthcor@gmail.com>
2024-12-17 11:22:33 +1100

    other updates

--- ghostty/set-config.zsh
+++ ghostty/set-config.zsh
@@ -2,5 +2,5 @@ cat <<EOF > ~/.config/ghostty/config
 macos-option-as-alt = left
 keybind = alt+left=esc:b
 keybind = alt+right=esc:f
-shell-integration-features = "no-cursor"
+shell-integration-features = no-cursor
 EOF
