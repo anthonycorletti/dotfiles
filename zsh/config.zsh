1b56d64923b2fc336e78af6046cb8067bb2b75c1
Anthony Corletti <anthcor@gmail.com>
2024-12-18 12:30:28 +1100

    fix bindkeys

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -24,9 +24,5 @@ setopt HIST_REDUCE_BLANKS # remove leading/trailing blanks from history lines
 #   like: git comm-[tab]
 setopt complete_aliases
 
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^?' backward-delete-char
+bindkey '\eb' backward-word
+bindkey '\ef' forward-word
