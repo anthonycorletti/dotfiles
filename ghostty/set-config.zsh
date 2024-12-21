7be45f48c24cabef3dcd3371d3ab5676f7f6894d
Anthony Corletti <anthcor@gmail.com>
2024-12-21 17:21:39 +1100

    wip

--- ghostty/set-config.zsh
+++ ghostty/set-config.zsh
@@ -1,6 +1,1 @@
-cat <<EOF > ~/.config/ghostty/config
-macos-option-as-alt = left
-keybind = alt+left=esc:b
-keybind = alt+right=esc:f
-shell-integration-features = no-cursor
-EOF
+cp $HOME/.dotfiles/ghostty/config ~/.config/ghostty/config
