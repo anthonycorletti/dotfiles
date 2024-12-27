7e1f92a8c4d45678e77d3158d74b9f225680b1f9
Anthony Corletti <anthcor@gmail.com>
2024-12-27 19:37:07 +1100

    more completions work

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -1,3 +1,14 @@
 vim.g.mapleader = " "
 vim.g.editorconfig = true
 vim.wo.number = true
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+-- Clear search highlights
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+vim.cmd("syntax enable")
