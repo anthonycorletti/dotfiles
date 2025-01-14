ed90c6ad96c497d152f21d01d26744455a8fb975
Anthony Corletti <anthcor@gmail.com>
2025-01-13 20:01:14 -0500

    apply formatting for certain types

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -5,7 +5,9 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.prettier.with({
+          filetypes = { "html", "json", "yaml", "markdown", "javascript", "typescript" },
+        }),
       },
     })
 
