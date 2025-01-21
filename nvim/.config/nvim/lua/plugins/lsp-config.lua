e7b07e12725c1856a7bb2db3e0d42a455b989360
Anthony Corletti <anthcor@gmail.com>
2025-01-21 16:35:01 -0500

    getting in the way

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -58,16 +58,6 @@ return {
         capabilities = capabilities,
       })
 
-      -- json
-      lspconfig.jsonls.setup({
-        capabilities = capabilities,
-      })
-
-      -- yaml
-      lspconfig.yamlls.setup({
-        capabilities = capabilities,
-      })
-
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
