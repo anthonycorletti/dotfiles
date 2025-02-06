0eb8d9089e0a2a28e64a7a5c97e5fe2ec5e827c5
Anthony Corletti <anthcor@gmail.com>
2025-02-06 10:51:19 -0500

    small keybinding updates

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -25,13 +25,6 @@ return {
           completion = cmp.config.window.bordered(),
           documentation = cmp.config.window.bordered(),
         },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
           { name = "luasnip" }, -- For luasnip users.
