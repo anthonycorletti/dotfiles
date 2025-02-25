10e1cbc785ffbc3b382dd5047b8ad19c03997c13
Anthony Corletti <anthcor@gmail.com>
2025-02-24 19:00:51 -0500

    some ⌘ keymaps

--- nvim/.config/nvim/lua/vim-keymaps.lua
+++ nvim/.config/nvim/lua/vim-keymaps.lua
@@ -10,3 +10,16 @@ keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
 -- Clear search highlights
 keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+
+-- Map D-z and D-Z to undo and redo
+keymap.set("n", "<D-z>", ":undo<CR>")
+keymap.set("n", "<D-Z>", ":redo<CR>")
+keymap.set("i", "<D-z>", "<ESC>:undo<CR>a")
+keymap.set("i", "<D-Z>", "<ESC>:redo<CR>a")
+
+-- Map D-s to save in normal and insert mode
+keymap.set("n", "<D-s>", ":w<CR>")
+keymap.set("i", "<D-s>", "<ESC>:w<CR>a")
+
+-- Map C-a to select all in the current buffer
+keymap.set("n", "<D-A>", "ggVG")
