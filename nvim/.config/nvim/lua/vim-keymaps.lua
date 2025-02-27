b3c86f538ffbf0aca2df550c07b2ee45e051ad2c
Anthony Corletti <anthcor@gmail.com>
2025-02-27 18:13:21 -0500

    keymapping update

--- nvim/.config/nvim/lua/vim-keymaps.lua
+++ nvim/.config/nvim/lua/vim-keymaps.lua
@@ -11,15 +11,32 @@ keymap.set("n", "<c-l>", ":wincmd l<CR>")
 -- Clear search highlights
 keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 
+-- Move the current line or selected lines up
+keymap.set("n", "<A-Up>", ":m .-2<CR>==", { noremap = true, silent = true })
+keymap.set("i", "<A-Up>", "<Esc>:m .-2<CR>==", { noremap = true, silent = true })
+keymap.set("v", "<A-Up>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+
+-- Move the current line or selected lines down
+keymap.set("n", "<A-Down>", ":m .+1<CR>==", { noremap = true, silent = true })
+keymap.set("i", "<A-Down>", "<Esc>:m .+1<CR>==", { noremap = true, silent = true })
+keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+
 -- Map D-z and D-Z to undo and redo
-keymap.set("n", "<D-z>", ":undo<CR>")
-keymap.set("n", "<D-Z>", ":redo<CR>")
-keymap.set("i", "<D-z>", "<ESC>:undo<CR>a")
-keymap.set("i", "<D-Z>", "<ESC>:redo<CR>a")
+keymap.set("n", "<D-z>", ":undo<CR>", { noremap = true, silent = true })
+keymap.set("n", "<D-Z>", ":redo<CR>", { noremap = true, silent = true })
+keymap.set("i", "<D-z>", "<Esc>:undo<CR>a", { noremap = true, silent = true })
+keymap.set("i", "<D-Z>", "<Esc>:redo<CR>a", { noremap = true, silent = true })
 
 -- Map D-s to save in normal and insert mode
-keymap.set("n", "<D-s>", ":w<CR>")
-keymap.set("i", "<D-s>", "<ESC>:w<CR>a")
+keymap.set("n", "<D-s>", ":w<CR>", { silent = true })
+keymap.set("i", "<D-s>", "<Esc>:w<CR>a", { silent = true })
 
 -- Map C-a to select all in the current buffer
 keymap.set("n", "<D-A>", "ggVG")
+keymap.set("i", "<D-A>", "<Esc>ggVG")
+keymap.set("v", "<D-A>", "<Esc>ggVG")
+
+-- Map D-f to search forward in normal and insert mode
+keymap.set("n", "<D-f>", ":%s/<C-r><C-w>//g<Left><Left>", { noremap = true })
+keymap.set("i", "<D-f>", "<Esc>:%s/<C-r><C-w>//g<Left><Left>", { noremap = true })
+keymap.set("v", "<D-f>", ":%s/<C-r><C-w>//g<Left><Left>", { noremap = true })
