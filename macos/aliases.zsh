fdd821f21860465a2433e1a70ac7812a88fa7332
Anthony Corletti <anthcor@gmail.com>
2025-03-01 14:40:59 -0500

    hell yeah this is awesome

--- macos/aliases.zsh
+++ macos/aliases.zsh
@@ -1,2 +1,3 @@
 alias showhidden='defaults write com.apple.finder AppleShowAllFiles TRUE && killall Finder'
 alias hidehidden='defaults write com.apple.finder AppleShowAllFiles FALSE && killall Finder'
+alias rem="reminders"
