bf70f660ecdc5ce711d6620703560b36c2170a53
Anthony Corletti <anthony@crosbylegal.com>
2025-06-07 14:53:30 -0400

    update gd alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -8,7 +8,7 @@ alias gcb='git copy-branch-name'
 alias gcl='git clone'
 alias gco='git checkout'
 alias gcom='git checkout $(gbm)'
-alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -R'
+alias gd='git diff "$@"'
 alias ge='git-edit-new'
 alias gf='git fetch'
 alias gl="git log --pretty=oneline -n 23 --graph"
