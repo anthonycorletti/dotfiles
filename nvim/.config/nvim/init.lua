15605254890e07ccc53ace94f7eb9df788cf058b
Anthony Corletti <anthcor@gmail.com>
2025-08-03 19:01:59 -0400

    cleanup

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -825,18 +825,9 @@ require("lazy").setup({
     version = "*",
     dependencies = "nvim-tree/nvim-web-devicons",
     config = function()
+      vim.opt.termguicolors = true
       require("bufferline").setup {
-        options = {
-          offsets = {
-            {
-              filetype = "neo-tree",
-              text = "File Explorer",
-              highlight = "Directory",
-              text_align = "left",
-              separator = true,
-            },
-          },
-        },
+        options = {}
       }
     end,
   },
