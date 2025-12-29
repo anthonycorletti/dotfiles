26d71a11f8228055da16eb3d21342525fe47156b
Anthony Corletti <anthcor@gmail.com>
2025-12-29 13:58:33 -0500

    remove this

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -3,14 +3,18 @@ return {
     "saghen/blink.cmp",
     event = "VimEnter",
     version = "1.*",
-    build = "cargo build --release",
     dependencies = {
       {
         "L3MON4D3/LuaSnip",
         version = "2.*",
         build = "make install_jsregexp",
         dependencies = {
-          { "rafamadriz/friendly-snippets", config = function() require("luasnip.loaders.from_vscode").lazy_load() end },
+          {
+            "rafamadriz/friendly-snippets",
+            config = function()
+              require("luasnip.loaders.from_vscode").lazy_load()
+            end,
+          },
         },
       },
       "folke/lazydev.nvim",
