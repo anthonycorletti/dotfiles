24471c1e176827cf82f64c8e25738daf8e981a9d
Anthony Corletti <anthcor@gmail.com>
2026-01-03 17:02:13 -0500

    updates

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -14,7 +14,9 @@ return {
       require("mini.surround").setup()
       local statusline = require("mini.statusline")
       statusline.setup({ use_icons = vim.g.have_nerd_font })
-      statusline.section_location = function() return "%2l:%-2v" end
+      statusline.section_location = function()
+        return "%2l:%-2v"
+      end
     end,
   },
 
@@ -39,27 +41,46 @@ return {
     "stevearc/conform.nvim",
     lazy = false,
     keys = {
-      { "<leader>f", function() require("conform").format({ async = true, lsp_format = "fallback" }) end, mode = "", desc = "Format buffer" },
+      {
+        "<leader>f",
+        function()
+          require("conform").format({ async = true, lsp_format = "fallback" })
+        end,
+        mode = "",
+        desc = "Format buffer",
+      },
     },
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
-        if vim.tbl_contains({ "c", "cpp" }, vim.bo[bufnr].filetype) then return nil end
         return { timeout_ms = 2500, lsp_format = "true" }
       end,
       formatters_by_ft = {
-        lua = { "stylua" }, python = { "ruff_format", "ruff_organize_imports" },
-        ruby = { "standardrb" }, go = { "gofmt" },
-        typescript = web_fmt, javascript = web_fmt, typescriptreact = web_fmt, javascriptreact = web_fmt,
-        json = web_fmt, html = web_fmt, css = web_fmt, scss = web_fmt, yaml = web_fmt, markdown = web_fmt,
+        lua = { "stylua" },
+        python = { "ruff_format", "ruff_organize_imports" },
+        ruby = { "standardrb" },
+        go = { "gofmt" },
+        typescript = web_fmt,
+        javascript = web_fmt,
+        typescriptreact = web_fmt,
+        javascriptreact = web_fmt,
+        json = web_fmt,
+        html = web_fmt,
+        css = web_fmt,
+        scss = web_fmt,
+        yaml = web_fmt,
+        markdown = web_fmt,
       },
     },
   },
 
   { "RRethy/vim-illuminate" },
 
-  { "github/copilot.vim", config = function()
-    vim.b.copilot_enabled = true
-    vim.g.copilot_filetypes = { markdown = false }
-  end },
+  {
+    "github/copilot.vim",
+    config = function()
+      vim.b.copilot_enabled = true
+      vim.g.copilot_filetypes = { markdown = false }
+    end,
+  },
 }
