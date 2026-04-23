88157d2eb93d62dd15a16ec7a8883ddfaf03ec84
Anthony Corletti <anthcor@gmail.com>
2026-04-23 19:22:03 -0400

    add ty

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -31,22 +31,36 @@ return {
 
           local client = vim.lsp.get_client_by_id(event.data.client_id)
           local supports = function(method)
-            if vim.fn.has("nvim-0.11") == 1 then return client:supports_method(method, event.buf)
-            else return client.supports_method(method, { bufnr = event.buf }) end
+            if vim.fn.has("nvim-0.11") == 1 then
+              return client:supports_method(method, event.buf)
+            else
+              return client.supports_method(method, { bufnr = event.buf })
+            end
           end
 
           if client and supports(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
             local group = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
-            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, { buffer = event.buf, group = group, callback = vim.lsp.buf.document_highlight })
-            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, { buffer = event.buf, group = group, callback = vim.lsp.buf.clear_references })
+            vim.api.nvim_create_autocmd(
+              { "CursorHold", "CursorHoldI" },
+              { buffer = event.buf, group = group, callback = vim.lsp.buf.document_highlight }
+            )
+            vim.api.nvim_create_autocmd(
+              { "CursorMoved", "CursorMovedI" },
+              { buffer = event.buf, group = group, callback = vim.lsp.buf.clear_references }
+            )
             vim.api.nvim_create_autocmd("LspDetach", {
               group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
-              callback = function(e) vim.lsp.buf.clear_references(); vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = e.buf }) end,
+              callback = function(e)
+                vim.lsp.buf.clear_references()
+                vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = e.buf })
+              end,
             })
           end
 
           if client and supports(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-            map("<leader>th", function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf })) end, "Toggle Inlay Hints")
+            map("<leader>th", function()
+              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+            end, "Toggle Inlay Hints")
           end
         end,
       })
@@ -54,17 +68,34 @@ return {
       vim.diagnostic.config({
         severity_sort = true,
         float = { border = "rounded", source = "if_many", header = "", prefix = "" },
-        underline = { severity = { vim.diagnostic.severity.ERROR, vim.diagnostic.severity.WARN, vim.diagnostic.severity.INFO, vim.diagnostic.severity.HINT } },
-        signs = vim.g.have_nerd_font and { text = { [vim.diagnostic.severity.ERROR] = "󰅚 ", [vim.diagnostic.severity.WARN] = "󰀪 ", [vim.diagnostic.severity.INFO] = "󰋽 ", [vim.diagnostic.severity.HINT] = "󰌶 " } } or {},
+        underline = {
+          severity = {
+            vim.diagnostic.severity.ERROR,
+            vim.diagnostic.severity.WARN,
+            vim.diagnostic.severity.INFO,
+            vim.diagnostic.severity.HINT,
+          },
+        },
+        signs = vim.g.have_nerd_font and {
+          text = {
+            [vim.diagnostic.severity.ERROR] = "󰅚 ",
+            [vim.diagnostic.severity.WARN] = "󰀪 ",
+            [vim.diagnostic.severity.INFO] = "󰋽 ",
+            [vim.diagnostic.severity.HINT] = "󰌶 ",
+          },
+        } or {},
         virtual_text = false,
       })
 
       local capabilities = require("blink.cmp").get_lsp_capabilities()
       local servers = {
-        gopls = {}, rust_analyzer = {}, ts_ls = {},
+        gopls = {},
+        rust_analyzer = {},
+        ts_ls = {},
         ruff = { settings = { args = { "--config=pyproject.toml" } } },
         lua_ls = { settings = { Lua = { completion = { callSnippet = "Replace" } } } },
         ruby_lsp = { settings = { rubyLsp = { formatter = "standard" } } },
+        ty = {},
       }
 
       local ensure_installed = vim.tbl_keys(servers)
