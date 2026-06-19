a22f1431703324fc914a8b57268b2b7bb9c6afe0
Anthony Corletti <anthony@crosbylegal.com>
2026-06-19 11:52:16 -0400

    updateS

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -11,12 +11,12 @@ return {
         enabled = true,
         sources = {
           files = {
-            hidden = true,   -- include dotfiles
-            ignored = true,  -- include .gitignore'd files
+            hidden = true,
+            ignored = false,
           },
           grep = {
             hidden = true,
-            ignored = true,
+            ignored = false,
           },
         },
       },
@@ -29,27 +29,163 @@ return {
     },
     keys = {
       -- Files
-      { "<leader>sf", function() Snacks.picker.files() end,                desc = "[S]earch [F]iles" },
-      { "<leader>sg", function() Snacks.picker.grep() end,                 desc = "[S]earch by [G]rep" },
-      { "<leader>se", function() Snacks.picker.grep({ regex = false }) end, desc = "[S]earch [E]xact (literal)" },
-      { "<leader>sw", function() Snacks.picker.grep_word() end,            desc = "[S]earch current [W]ord" },
-      { "<leader>s.", function() Snacks.picker.recent() end,               desc = "[S]earch Recent Files" },
-      { "<leader>sr", function() Snacks.picker.resume() end,               desc = "[S]earch [R]esume" },
-      { "<leader>sk", function() Snacks.picker.keymaps() end,              desc = "[S]earch [K]eymaps" },
-      { "<leader>ss", function() Snacks.picker.pickers() end,              desc = "[S]earch [S]elect picker" },
-      { "<leader>sd", function() Snacks.picker.diagnostics() end,          desc = "[S]earch [D]iagnostics" },
-      { "<leader>sn", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "[S]earch [N]eovim config" },
-      { "<leader><leader>", function() Snacks.picker.buffers() end,        desc = "Find buffers" },
-      { "<leader>/",  function() Snacks.picker.lines() end,                desc = "Fuzzy search buffer" },
-      { "<leader>s/", function() Snacks.picker.grep({ search_dirs = vim.tbl_map(function(b) return vim.api.nvim_buf_get_name(b) end, vim.api.nvim_list_bufs()) }) end, desc = "[S]earch in Open Files" },
+      {
+        "<leader>sf",
+        function()
+          Snacks.picker.files()
+        end,
+        desc = "[S]earch [F]iles",
+      },
+      {
+        "<leader>sF",
+        function()
+          Snacks.picker.files({ ignored = true })
+        end,
+        desc = "[S]earch [F]iles (all files)",
+      },
+
+      -- Grep
+      {
+        "<leader>sg",
+        function()
+          Snacks.picker.grep()
+        end,
+        desc = "[S]earch by [G]rep",
+      },
+      {
+        "<leader>sG",
+        function()
+          Snacks.picker.grep({ ignored = true })
+        end,
+        desc = "[S]earch by [G]rep (all files)",
+      },
+      {
+        "<leader>se",
+        function()
+          Snacks.picker.grep({ regex = false })
+        end,
+        desc = "[S]earch [E]xact (literal)",
+      },
+      {
+        "<leader>sw",
+        function()
+          Snacks.picker.grep_word()
+        end,
+        desc = "[S]earch current [W]ord",
+      },
+      {
+        "<leader>s.",
+        function()
+          Snacks.picker.recent()
+        end,
+        desc = "[S]earch Recent Files",
+      },
+      {
+        "<leader>sr",
+        function()
+          Snacks.picker.resume()
+        end,
+        desc = "[S]earch [R]esume",
+      },
+      {
+        "<leader>sk",
+        function()
+          Snacks.picker.keymaps()
+        end,
+        desc = "[S]earch [K]eymaps",
+      },
+      {
+        "<leader>ss",
+        function()
+          Snacks.picker.pickers()
+        end,
+        desc = "[S]earch [S]elect picker",
+      },
+      {
+        "<leader>sd",
+        function()
+          Snacks.picker.diagnostics()
+        end,
+        desc = "[S]earch [D]iagnostics",
+      },
+      {
+        "<leader>sn",
+        function()
+          Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
+        end,
+        desc = "[S]earch [N]eovim config",
+      },
+
+      {
+        "<leader><leader>",
+        function()
+          Snacks.picker.buffers()
+        end,
+        desc = "Find buffers",
+      },
+      {
+        "<leader>/",
+        function()
+          Snacks.picker.lines()
+        end,
+        desc = "Fuzzy search buffer",
+      },
+
+      {
+        "<leader>s/",
+        function()
+          Snacks.picker.grep({
+            search_dirs = vim.tbl_map(function(b)
+              return vim.api.nvim_buf_get_name(b)
+            end, vim.api.nvim_list_bufs()),
+          })
+        end,
+        desc = "[S]earch in Open Files",
+      },
 
       -- LSP
-      { "grr", function() Snacks.picker.lsp_references() end,      desc = "LSP References" },
-      { "gri", function() Snacks.picker.lsp_implementations() end, desc = "LSP Implementations" },
-      { "grd", function() Snacks.picker.lsp_definitions() end,     desc = "LSP Definitions" },
-      { "grt", function() Snacks.picker.lsp_type_definitions() end, desc = "LSP Type Definitions" },
-      { "gO",  function() Snacks.picker.lsp_symbols() end,         desc = "LSP Document Symbols" },
-      { "gW",  function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+      {
+        "grr",
+        function()
+          Snacks.picker.lsp_references()
+        end,
+        desc = "LSP References",
+      },
+      {
+        "gri",
+        function()
+          Snacks.picker.lsp_implementations()
+        end,
+        desc = "LSP Implementations",
+      },
+      {
+        "grd",
+        function()
+          Snacks.picker.lsp_definitions()
+        end,
+        desc = "LSP Definitions",
+      },
+      {
+        "grt",
+        function()
+          Snacks.picker.lsp_type_definitions()
+        end,
+        desc = "LSP Type Definitions",
+      },
+      {
+        "gO",
+        function()
+          Snacks.picker.lsp_symbols()
+        end,
+        desc = "LSP Document Symbols",
+      },
+      {
+        "gW",
+        function()
+          Snacks.picker.lsp_workspace_symbols()
+        end,
+        desc = "LSP Workspace Symbols",
+      },
     },
   },
 }
