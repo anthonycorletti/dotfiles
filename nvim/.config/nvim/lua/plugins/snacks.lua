e6489d29425eda9d7ef4947cbbdd3190f0597070
Anthony Corletti <anthcor@gmail.com>
2026-06-27 17:06:56 -0400

    updates

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -6,6 +6,12 @@ return {
     opts = {
       dashboard = {
         enabled = true,
+        sections = {
+          { section = "startup", indent = 2, padding = 1 },
+          { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        },
       },
       picker = {
         enabled = true,
