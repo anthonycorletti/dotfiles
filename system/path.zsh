a171eeaf2bc497f615f379bcc9fff27aead037da
Anthony Corletti <anthcor@gmail.com>
2026-08-30 17:39:29 -0400

    add local bin

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,2 @@
-export PATH="/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="$HOME/.local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
